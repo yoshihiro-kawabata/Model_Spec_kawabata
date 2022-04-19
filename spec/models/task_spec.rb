7f498b61776d4680e7180cc49e9ffb0b6a19ddcc
yoshihiro_kawabata <yoshihiro_kawabata@mediado.jp>
2022-04-19 15:23:22 +0900

    Model_Spec実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,48 +4,32 @@ RSpec.describe Task, type: :model do
   describe "バリデーション" do
     it "タイトルが未入力の場合、タスクのバリデーションが無効であること" do
       task = Task.new(title: nil, description: "test", status: :todo, deadline: Time.current)
-      #      バリデーションエラー検証
-      #      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current)
       expect(task).to be_invalid
       expect(task.errors.full_messages).to eq ["Title can't be blank"]
-      #      エラーメッセージ検証
-      #      expect(task.errors.full_messages).to eq ["Title can be blank"]
     end
 
     it "ステータスが未入力の場合、タスクのバリデーションが無効であること" do
       task = Task.new(title: "test", description: "test", status: nil, deadline: Time.current)
-      #      バリデーションエラー検証
-      #      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current)
       expect(task).to be_invalid
       expect(task.errors.full_messages).to eq ["Status can't be blank"]
-      #      エラーメッセージ検証
-      #      expect(task.errors.full_messages).to eq ["Status can be blank"]
     end
 
     it "完了期限が未入力の場合、タスクのバリデーションが無効であること" do
       task = Task.new(title: "test", description: "test", status: :todo, deadline: nil)
-      #      バリデーションエラー検証
-      #      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current)
       expect(task).to be_invalid
       expect(task.errors.full_messages).to eq ["Deadline can't be blank"]
-      #      エラーメッセージ検証
-      #      expect(task.errors.full_messages).to eq ["Deadline can be blank"]
     end
 
     it "完了期限が過去の日付の場合、タスクのバリデーションが無効であること" do
       task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.yesterday)
-      #      バリデーションエラー検証
-      #      task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.today)
       expect(task).to be_invalid
-      expect(task.deadline.strftime("%Y年 %m月 %d日")).to be < Date.today.strftime("%Y年 %m月 %d日")
+      expect(task.errors.full_messages).to eq ["Deadline must start from today."]
     end
 
     it "完了期限が今日の日付の場合、タスクのバリデーションが有効であること" do
       task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.today)
-      #      バリデーションエラー検証
-      #      task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.yesterday)
       expect(task).to be_valid
-      expect(task.deadline.strftime("%Y年 %m月 %d日")).to eq Date.today.strftime("%Y年 %m月 %d日")
+      expect(task.deadline).to eq Date.today
     end
   end
 end
